65823b0f7fc5f14aaa50b2e7938b5f6f8fb2925c
Difficultworld24 <kar232442@gmail.com>
2022-10-22 11:20:13 +0530

    Tailwind Css configured

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-slate-700">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,6 +13,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          By learning Vue
         </a>
       </header>
     </div>
